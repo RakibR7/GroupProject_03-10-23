6692c02a94e2c1625c2d10cfccda3643ec7287ff
Rakib <G00408644@atu.ie>
2023-10-03 15:31:07 +0100

    Hello World SpringBoot

---
+++ src/main/java/ie/atu/helloworldspringbot/HelloWorldSpringbotApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.helloworldspringbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/helloworldspringbot/HelloWorldSpringbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.helloworldspringbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
